701eec0f4b9d5f40ed25c4c75359994d719eb823
Munna <munnavai@gmail.com>
2018-09-10 22:00:29 +0600

    modified Hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(string[] args) {
-        System.out.println("VS Code is Awesome...")
+        System.out.println("Vs Code is Awesome...")
     }
 
 }
