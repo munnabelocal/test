a9f721f0b79072e1a1dd63e1dfc791985afe6fa8
Munna <munnavai@gmail.com>
2018-09-10 22:09:29 +0600

    Added Greetings Function to Hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,12 @@
 public class Hello {
     public static void main(string[] args) {
         System.out.println("Vs Code is Awesome...")
+
+        greetings("M.M.Rahman Munna");
     }
+
+public static void greetings(string name) {
+    System.out.println("Welcome " + name);
+}
 
 }
